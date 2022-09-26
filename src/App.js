803afc18291605d79ba8b5fd1db0180ee5e3cdbc
Aswanth Raveendran E K <aswanthek1@gmail.com>
2022-09-26 12:42:44 +0530

    basic design added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import './app.css'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
 
 function App() {
   return (
     <div className="App">
-   <h1>Hello World</h1>
+   <Navbar/>
+   <Banner/>
+   <RowPost/>
     </div>
   );
 }
